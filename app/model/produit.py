f087bffc336aec494a0adae38e44aceeac9ea7ad
hibaallahinfo <hibaallahessarghi@gmail.com>
2024-10-07 17:04:21 +0100

    produit

---
+++ app/model/produit.py
@@ -0,0 +1,16 @@
+class Produit:
+    def _init_(self, id, nom, quantite, description, prix):
+        self.id = id
+        self.nom = nom
+        self.quantite = quantite
+        self.description = description
+        self.prix = prix
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nom": self.nom,
+            "quantite": self.quantite,
+            "description": self.description,
+            "prix": self.prix
+        }
