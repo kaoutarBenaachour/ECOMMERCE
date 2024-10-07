b9ca90db7c40764e2fe487af17f582c97fce723c
ZinebZahir <zanoubazahir@gmail.com>
2024-10-07 17:46:23 +0200

    route addproduct

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, jsonify
 from app.service.deleteproduct import deleteproduct  # Corrected path to deleteproduct function
+from app.service.addProduct import add_product  # Corrected path to addproduct function
+
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -11,3 +14,9 @@ def index():
 def delete_product_route(id):
     result = deleteproduct(id)  # Call the deleteproduct function with the product ID
     return jsonify({"message": result})  # Return the result as a JSON response
+
+# Route for adding a product
+@main.route('/add_product/<int:id>', methods=['ADD'])
+def add_product_route(id):
+    result = add_product(id) 
+    return jsonify({"message": result})  
